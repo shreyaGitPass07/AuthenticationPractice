f5e970e603b4e55ceca20cd800b4116d68c6769c
Shreya Tiwari <shreyatiwari0861@gmail.com>
2026-03-19 20:21:40 +0530

    Added sign up and sign in

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle password visibility
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+  const type = passwordInput.type === "password" ? "text" : "password";
+  passwordInput.type = type;
+});
