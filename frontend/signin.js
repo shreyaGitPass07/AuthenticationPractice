1a83f42a99518a30227f09fee4566d1f2bc89960
Shreya Tiwari <shreyatiwari0861@gmail.com>
2026-03-24 16:38:03 +0530

    Updated

--- frontend/signin.js
+++ frontend/signin.js
@@ -19,7 +19,7 @@ form.addEventListener("submit", async (e) =>{
   
 
   try{
-    const response = await fetch("http://localhost:3000/api/login",{
+    const response = await fetch("https://authenticationpractice.onrender.com/api/login",{
       method : "POST",
       headers : {
         "Content-Type" : "application/json",
